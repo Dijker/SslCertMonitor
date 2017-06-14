ed624fded937bd547712ace1f30f41f4232d61ef
Gr33nDrag0n <git@gr33ndrag0n.com>
2017-06-13 21:44:15 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,83 @@
+Stand-alone PowerShell script to do end-user monitoring on SSL certificate(s) expiration date. It uses PowerShell so it run only on Windows.
+
+##**Installation**
+
+Save a copy of SslCertMonitor.ps1
+
+This README use this path C:\Scripts\SslCertMonitor.ps1, adjust accordingly if you use something else.
+
+##**Configuration, Manual Usage & Testing**
+
+Open the script in your favorite text editor. Basic Notepad WILL work but not recommended. I recommend notepad++ available for free [HERE](https://notepad-plus-plus.org/).
+
+Scroll to line #61
+
+> Configurable Variables | MANDATORY !!! EDIT THIS SECTION !!!
+
+The configuration is splitted in sub-sections:
+
+ - Url List
+ - E-mail
+
+### **Configuring URL Section**
+
+Replace the example domains by the urls you want to monitor.
+ 
+### **Configuring E-mail Section**
+
+In this section we will configure the address used to send and received the monitoring automatic e-mails.
+
+Config.          | Description                                                                    | Value Example
+------------     | -------------                                                                  | -------------
+SenderEmail      | This is the e-mail that will be used as sender by the script.                  | = 'LiskMonitor@mydomain.com'
+SenderSmtp       | This is the domain or IP address the script will use as SMTP to send messages. | = 'smtp.myinternetprovider.com'
+SendErrorMail    | Enable/Disable the sending of errors messages.                                 | = $True
+ErrorEmailList   | E-mail List                                                                    | = @('home@mydomain.com', '1234567890@phoneprovider.com')
+ 
+#### Test E-mail Configuration
+
+`.\SslCertMonitor.ps1 -SendTestEmail`
+
+#### Run manually the check
+
+`.\SslCertMonitor.ps1 -ShowMessage`
+
+##**Scheduled Task(s) Creation & Testing**
+
+The script do a check and send e-mail if necessary.
+
+It's not a program, so like you would use crontab in Linux, you need to use Task Scheduler in Windows
+
+Open Task Scheduler and Create a New Task. (Use Full GUI not the Wizard)
+
+General -> Name: SslCertMonitor
+
+General -> Execute even if user is not connected
+
+Trigger -> New
+
+			* Daily (Adjust Time of Execution)
+			
+Action  -> New
+
+			* Command:          C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+			* Arguments:        C:\Scripts\SslCertMonitor.ps1
+			* WorkingDirectory: C:\Scripts\
+
+##**Troubleshooting & Common Error(s)**
+
+**It doesn’t work.**
+
+Verify PowerShell version installed in your computer. Execute the following command:
+
+> $PSVersionTable
+
+The PSVersion must be at least v4.x.
+If not, go [HERE](https://www.microsoft.com/en-us/download/details.aspx?id=40855), select your language, download, install and reboot.
+
+When done re-run the test to confirm your version is now v4.x or upper.
+
+**Script is asking confirmation to execute when running it.**
+
+`Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass`
+
---
+++ SslCertMonitor.ps1
@@ -0,0 +1,194 @@
+<#
+.SYNOPSIS
+	Test SSL Certificate(s) Expiry.
+	
+.DESCRIPTION
+	Verify expiration of SSL certificate and send mail alert if under threshold.
+	
+.PARAMETER ShowMessage
+	Output message to screen. (Doesn't affect e-mail functionnality)
+
+.PARAMETER SendTestEmail
+	Send a test e-mail to the configured e-mails ERROR.
+
+.EXAMPLE
+	.\SslCertMonitor.ps1
+	
+	Normal run built to be executed by a scheduled task.
+	
+.EXAMPLE
+	.\SslCertMonitor.ps1 -ShowMessage
+	
+	To see on-screen output when script is runned manually.
+	
+.EXAMPLE
+	.\SslCertMonitor.ps1 -SendTestEmail
+	
+	To execute the script in e-mail test mode.
+	
+.NOTES
+	Version :	1.0.0.0
+	Author  :	Gr33nDrag0n
+	History :	2017/06/13 - v1.0.0.0 Last Modification
+				2017/06/12 - Creation
+#>
+
+###########################################################################################################################################
+### Parameters
+###########################################################################################################################################
+
+[CmdletBinding()]
+Param(
+	[parameter( Mandatory=$False )]
+	[switch] $ShowMessage,
+	
+	[parameter( Mandatory=$False )]
+	[switch] $SendTestEmail
+	)
+
+###########################################################################################################################################
+### Host Initialization
+###########################################################################################################################################
+
+[System.GC]::Collect()
+$error.Clear()
+
+# Disabling the cert. validation check for invalid certs support.
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+
+#######################################################################################################################
+# Configurable Variables | MANDATORY !!! EDIT THIS SECTION !!!
+#######################################################################################################################
+
+$Private:Banner = "SslCertMonitor v1.0 by Gr33nDrag0n"
+$Private:MinimumCertAgeDays = 14
+$Private:TimeoutMilliseconds = 3000
+
+### URL List ###============================================================================
+
+$Private:UrlList = @()
+
+$UrlList += 'https://www.lisknode.io'
+$UrlList += 'https://www.arknode.net'
+$UrlList += 'https://lisk.io'
+$UrlList += 'https://ark.io'
+$UrlList += 'https://lisktools.io'
+
+### E-Mail ###===============================================================================
+
+$Private:Email = @{}
+
+$Email.SenderEmail      = 'SslCertMonitor@mydomain.com'
+# Same SMTP address you would use in your e-mail client
+$Email.SenderSmtp       = 'smtp.myISP.com'
+$Email.SendErrorMail    = $True
+$Email.ErrorEmailList   = @('myemail@domain.com','my-2nd-optional-email@domain.com')
+
+
+###########################################################################################################################################
+# MAIN
+###########################################################################################################################################
+
+if( $SendTestEmail )
+{
+	Write-Host "`r`n$Banner`r`n`r`n" -ForegroundColor Green
+	Write-Host 'Sending Test Email(s)...'
+	
+	if( $Email.SendErrorMail -eq $True )
+	{
+		Send-MailMessage -SmtpServer $Email.SenderSmtp -From $Email.SenderEmail -To $Email.ErrorEmailList -Subject 'SslCertMonitor' -Body 'SslCertMonitor (SendTestEmail)' -Priority High
+	}
+	else
+	{
+		Write-Host '$Email.SendErrorMail is set to False, Skipping ERROR Email Test.'
+	}
+	
+	Write-Host "Done`r`n"
+}
+else
+{
+	if( $ShowMessage ) { Write-Host "`r`n$Banner`r`n`r`n" -ForegroundColor Green }
+	
+	$Private:AlertMessage = ''
+
+	ForEach( $Private:Url in $UrlList )
+	{
+		Try
+		{
+			$Private:Request = [System.Net.HttpWebRequest]::Create($Url)
+			$Request.Timeout = $TimeoutMilliseconds
+		}
+		Catch
+		{
+			Write-Warning "Invalid URL! => $Url"
+		}
+
+		
+		if( $Request )
+		{
+			Try
+			{
+				$Request.GetResponse().Dispose()
+				$Certificate = $Request.ServicePoint.Certificate		
+
+			}
+			Catch
+			{
+				Write-Warning "Fetching of the certificate FAILED! => $Url"
+			}
+		}
+		
+		if( $Certificate )
+		{
+			#$Private:CertInfo = @{}
+			
+			#$CertInfo.Address = $Url
+			#$CertInfo.Subject = $Certificate.Subject
+			#$CertInfo.Name = $Certificate.GetName()
+			#$CertInfo.EffectiveDate = $Certificate.GetEffectiveDateString()
+			#$CertInfo.ExpirationDate = $Certificate.GetExpirationDateString()
+			#$CertInfo.ExpirationDayCount = ( $( [datetime]$CertInfo.ExpirationDate ) - $( Get-Date ) ).Days
+			#$CertInfo.Issuer = $Certificate.GetIssuerName()
+			#$CertInfo.KeyAlgorithm = $Certificate.GetKeyAlgorithm()
+			#$CertInfo.KeyAlgorithmParameters = $Certificate.GetKeyAlgorithmParametersString()
+			#$CertInfo.PublicKey = $Certificate.GetPublicKeyString()
+			#$CertInfo.RawCert = $Certificate.GetRawCertDataString()
+			#$CertInfo.Serial = $Certificate.GetSerialNumberString()
+			#$CertInfo.Hash = $Certificate.GetCertHashString()
+			
+			$Private:ExpirationDate = $Certificate.GetExpirationDateString()
+			$Private:DayCount = ( $( [datetime]$ExpirationDate ) - $( Get-Date ) ).Days
+
+			if( $DayCount -le $MinimumCertAgeDays )
+			{
+				$AlertMessage += "$DayCount day(s) left for $Url`r`n"
+				if( $ShowMessage ) { Write-Host "$DayCount | $Url" -ForegroundColor Red }
+			}
+			else
+			{
+				if( $ShowMessage ) { Write-Host "$DayCount | $Url" -ForegroundColor Green }
+			}
+		}
+	}
+
+	### E-mail Reporting ###=======================================================================================
+
+	if( $Email.SendErrorMail -eq $True )
+	{
+		if( $AlertMessage -ne '' )
+		{
+			if( $ShowMessage ) { Write-Host "`r`nExpiring certificate(s) detected, sending alert mail.`r`n" -ForegroundColor Red }
+			
+			Send-MailMessage -SmtpServer $Email.SenderSmtp -From $Email.SenderEmail -To $Email.ErrorEmailList -Subject 'SslCertMonitor Alert' -Body $AlertMessage -Priority High
+		}
+		else
+		{
+			if( $ShowMessage ) { Write-Host "`r`nNo expiring certificate(s) detected, skipping alert mail.`r`n" -ForegroundColor Green }
+		}
+	}
+	else
+	{
+		if( $ShowMessage ) { Write-Host '`r`nSendErrorMail = $False, Skipping Email/SMS.`r`n' -ForegroundColor Yellow }
+	}
+}
